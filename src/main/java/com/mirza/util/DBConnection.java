f6ada7323861744208ac541f97c911300e7bf38f
Ahmedbaig Inamdar <ahmedbaig@gmail.com>
2026-03-02 21:16:44 +0530

    Fixed servlet API to javax

--- src/main/java/com/mirza/util/DBConnection.java
+++ src/main/java/com/mirza/util/DBConnection.java
@@ -2,15 +2,12 @@ package com.mirza.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DBConnection {
 
-	private static final String URL =
-			"\r\n"
-			+ "postgresql://postgres:KUDXIfyrhHiLtxiuGoJyeUzdiLRRmbcn@postgres.railway.internal:5432/railway";
-
-    private static final String USER = "railway";
+    // Using the internal Railway host for better performance
+    private static final String URL = "jdbc:postgresql://postgres.railway.internal:5432/railway";
+    private static final String USER = "postgres";
     private static final String PASS = "KUDXIfyrhHiLtxiuGoJyeUzdiLRRmbcn";
 
     public static Connection getConnection() {
@@ -18,6 +15,8 @@ public class DBConnection {
             Class.forName("org.postgresql.Driver");
             return DriverManager.getConnection(URL, USER, PASS);
         } catch (Exception e) {
+            // This will show up in your 'Deploy Logs' if the connection fails
+            System.err.println("DB Connection Error: " + e.getMessage());
             e.printStackTrace();
             return null;
         }
